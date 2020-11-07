f8503783a679a9ca1a327cfe43d43da82b74fdcb
Rodrigo Costa Santos <rodrigocostasantos07@gmail.com>
2020-11-07 12:57:04 -0200

    init project

---
+++ src/main/java/com/seed/SeedJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedJavaSpringbootApplication.class, args);
+	}
+
+}
