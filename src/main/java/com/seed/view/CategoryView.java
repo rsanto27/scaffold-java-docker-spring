f3836f110f4f0c6cfc8d8fca00507fc52c08ac34
Rodrigo Costa Santos <rodrigocostasantos07@gmail.com>
2020-11-07 13:35:12 -0200

    testing the first model class

--- src/main/java/com/seed/view/CategoryView.java
+++ src/main/java/com/seed/view/CategoryView.java
@@ -1,16 +1,29 @@
 package com.seed.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.seed.model.CategoryModel;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryView {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String toList() {
-		return "Rest layer is working fine";
+	public List<CategoryModel> toList() {
+		
+		CategoryModel category1 = new CategoryModel(1, "Computing");
+		CategoryModel category2 = new CategoryModel(2, "Office");
+		
+		List<CategoryModel> list = new ArrayList<CategoryModel>();
+		list.add(category1);
+		list.add(category2);
+		
+		return list;
 	}
 
 }
