7f7df9f4a256c7b4781ceded1617e055bda32aab
Rodrigo Costa Santos <rodrigocostasantos07@gmail.com>
2020-11-08 19:35:22 -0200

    creating insert operation

--- src/main/java/com/seed/SeedJavaSpringbootApplication.java
+++ src/main/java/com/seed/SeedJavaSpringbootApplication.java
@@ -1,13 +1,32 @@
 package com.seed;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.seed.domain.CategoryDomain;
+import com.seed.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class SeedJavaSpringbootApplication {
+public class SeedJavaSpringbootApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeedJavaSpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		CategoryDomain cat1 = new CategoryDomain(null, "Office");
+		CategoryDomain cat2 = new CategoryDomain(null, "Computing");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
