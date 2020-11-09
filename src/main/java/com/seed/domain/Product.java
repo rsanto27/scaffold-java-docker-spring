a382133d7b7070391e981f8d514f948c1d7cc07f
Rodrigo Costa Santos <rodrigocostasantos07@gmail.com>
2020-11-09 12:24:23 -0200

    fix way to handler with notFound

--- src/main/java/com/seed/domain/Product.java
+++ src/main/java/com/seed/domain/Product.java
@@ -13,6 +13,8 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 @Table(name = "product")
 public class Product implements Serializable {
@@ -24,6 +26,7 @@ public class Product implements Serializable {
 	private Integer id;
 	private String name;
 	private Double price;
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "product_category",
 		joinColumns = @JoinColumn(name = "product_id"),
--- src/main/java/com/seed/services/CategoryService.java
+++ src/main/java/com/seed/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.seed.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.seed.domain.Category;
 import com.seed.repositories.CategoryRepository;
+import com.seed.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
@@ -13,7 +16,9 @@ public class CategoryService {
 	private CategoryRepository catRepo;
 	
 	public Category find(Integer id) {
-		return catRepo.findById(id).orElse(null);
+		Optional<Category> category = catRepo.findById(id);
+		return category.orElseThrow(() -> new ObjectNotFoundException(
+				"Not found. Id: " + id + ", Type: " + Category.class.getName()));
 	}
 
 }
