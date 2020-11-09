1f97f02737fbf0a65b5972a2e9cf1ac66f047f2f
Rodrigo Costa Santos <rodrigocostasantos07@gmail.com>
2020-11-09 12:42:53 -0200

    init model of state vs city

---
+++ src/main/java/com/seed/domain/City.java
@@ -0,0 +1,68 @@
+package com.seed.domain;
+
+public class City {
+	
+	private Integer id;
+	private String name;
+	private State state;
+	
+	public City() {
+		super();
+	}
+	
+	public City(Integer id, String name, State state) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.state = state;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		City other = (City) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/com/seed/domain/State.java
@@ -0,0 +1,71 @@
+package com.seed.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class State {
+	
+	private Integer id;
+	private String name;
+	private List<City> cities = new ArrayList<City>();
+	
+	public State() {
+		super();
+	}
+
+	public State(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<City> getCities() {
+		return cities;
+	}
+	
+	public void setCities(List<City> cities) {
+		this.cities = cities;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
